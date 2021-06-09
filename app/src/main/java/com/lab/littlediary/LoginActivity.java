29676ea5814e80cb7a1941510e8180f8a83160eb
Starakira <bangunagung1@gmail.com>
2021-06-09 20:01:08 +0700

    Authentication update

--- app/src/main/java/com/lab/littlediary/LoginActivity.java
+++ app/src/main/java/com/lab/littlediary/LoginActivity.java
@@ -1,14 +1,92 @@
 package com.lab.littlediary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText emailFormLogin, passwordFormLogin;
+    Button btnLogin;
+    TextView toRegisterTextView;
+    ProgressBar progressBarLogin;
+
+    FirebaseAuth fAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_login);
+
+        emailFormLogin = findViewById(R.id.emailLogin);
+        passwordFormLogin = findViewById(R.id.passwordLogin);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        toRegisterTextView = findViewById(R.id.registerTextLogin);
+
+        progressBarLogin = findViewById(R.id.progressBarLogin);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailFormLogin.getText().toString().trim();
+                String password = passwordFormLogin.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    emailFormLogin.setError("Email is required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    passwordFormLogin.setError("Password is required");
+                    return;
+                }
+
+                progressBarLogin.setVisibility(View.VISIBLE);
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        toRegisterTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/lab/littlediary/RegisterActivity.java
+++ app/src/main/java/com/lab/littlediary/RegisterActivity.java
@@ -1,14 +1,103 @@
 package com.lab.littlediary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText nameFormRegister, emailFormRegister, passwordFormRegister;
+    Button btnRegister;
+    TextView toLoginTextView;
+    ProgressBar progressBarRegister;
+
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_register);
+
+        nameFormRegister = findViewById(R.id.nameRegister);
+        emailFormRegister = findViewById(R.id.emailRegister);
+        passwordFormRegister = findViewById(R.id.passwordRegister);
+
+        btnRegister = findViewById(R.id.btnRegister);
+
+        toLoginTextView = findViewById(R.id.loginTextRegister);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        progressBarRegister = findViewById(R.id.progressBarRegister);
+
+        if (fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailFormRegister.getText().toString().trim();
+                String password = passwordFormRegister.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    emailFormRegister.setError("Email is required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    passwordFormRegister.setError("Password is required");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    passwordFormRegister.setError("Password must be more than 6 characters long");
+                    return;
+                }
+
+                progressBarRegister.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        toLoginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
